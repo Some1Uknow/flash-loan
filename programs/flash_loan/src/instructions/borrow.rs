e7b525e35f820fc32d313f7603365ffc3e9157fe
Some1UKnow <raghu250407@gmail.com>
2026-04-29 16:09:07 +0530

    Refactor flash loan instructions: separate borrow and repay logic into handlers, remove unused code, and add instruction module.

--- programs/flash_loan/src/instructions/borrow.rs
+++ programs/flash_loan/src/instructions/borrow.rs
@@ -1,11 +1,77 @@
+use anchor_lang::prelude::*;
+use anchor_lang::Discriminator;
+use anchor_spl::token::{transfer, Transfer};
 
-  use anchor_lang::prelude::*;
-  use anchor_lang::Discriminator;
-  use anchor_spl::token::{transfer, Transfer};
+use crate::{instruction, Loan, ProtocolError, ID};
 
-  use crate::{instruction, Loan, ProtocolError, ID};
+pub fn handler(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
+    // Make sure we're not sending in an invalid amount that can crash our Protocol
+    require!(borrow_amount > 0, ProtocolError::InvalidAmount);
 
-  pub fn handler(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
-      
-  }
+    // Derive the Signer Seeds for the Protocol Account
+    let seeds = &[b"protocol".as_ref(), &[ctx.bumps.protocol]];
+    let signer_seeds = &[&seeds[..]];
 
+    // Transfer the funds from the protocol to the borrower
+    transfer(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.key(),
+            Transfer {
+                from: ctx.accounts.protocol_ata.to_account_info(),
+                to: ctx.accounts.borrower_ata.to_account_info(),
+                authority: ctx.accounts.protocol.to_account_info(),
+            },
+            signer_seeds,
+        ),
+        borrow_amount,
+    )?;
+
+    let ixs = ctx.accounts.instructions.to_account_info();
+
+    /*
+        Repay Instruction Check
+
+        Make sure that the last instruction of this transaction is a repay instruction
+    */
+
+    // Check if this is the first instruction in the transaction.
+    let current_index = load_current_index_checked(&ctx.accounts.instructions)?;
+    require_eq!(current_index, 0, ProtocolError::InvalidIx);
+
+    // Check how many instruction we have in this transaction
+    let instruction_sysvar = ixs.try_borrow_data()?;
+    let len = u16::from_le_bytes(instruction_sysvar[0..2].try_into().unwrap());
+
+    // Ensure we have a repay ix
+    if let Ok(repay_ix) = load_instruction_at_checked(len as usize - 1, &ixs) {
+        // Instruction checks
+        require_keys_eq!(repay_ix.program_id, ID, ProtocolError::InvalidProgram);
+        require!(
+            repay_ix.data[0..8].eq(instruction::Repay::DISCRIMINATOR),
+            ProtocolError::InvalidIx
+        );
+
+        // We could check the Wallet and Mint separately but by checking the ATA we do this automatically
+        require_keys_eq!(
+            repay_ix
+                .accounts
+                .get(3)
+                .ok_or(ProtocolError::InvalidBorrowerAta)?
+                .pubkey,
+            ctx.accounts.borrower_ata.key(),
+            ProtocolError::InvalidBorrowerAta
+        );
+        require_keys_eq!(
+            repay_ix
+                .accounts
+                .get(4)
+                .ok_or(ProtocolError::InvalidProtocolAta)?
+                .pubkey,
+            ctx.accounts.protocol_ata.key(),
+            ProtocolError::InvalidProtocolAta
+        );
+    } else {
+        return Err(ProtocolError::MissingRepayIx.into());
+    }
+    Ok(())
+}
--- programs/flash_loan/src/instructions/repay.rs
+++ programs/flash_loan/src/instructions/repay.rs
@@ -1,11 +1,44 @@
+use anchor_lang::prelude::*;
+use anchor_lang::Discriminator;
+use anchor_spl::token::{transfer, Transfer};
 
-  use anchor_lang::prelude::*;
-  use anchor_lang::Discriminator;
-  use anchor_spl::token::{transfer, Transfer};
+use crate::{instruction, Loan, ProtocolError, ID};
 
-  use crate::{instruction, Loan, ProtocolError, ID};
+pub fn handler(ctx: Context<Loan>) -> Result<()> {
+    let ixs = ctx.accounts.instructions.to_account_info();
 
-  pub fn handler(ctx: Context<Loan>, borrow_amount: u64) -> Result<()> {
-      
-  }
+    let mut amount_borrowed: u64;
 
+    if let Ok(borrow_ix) = load_instruction_at_checked(0, &ixs) {
+        // Check the amount borrowed:
+        let mut borrowed_data: [u8; 8] = [0u8; 8];
+        borrowed_data.copy_from_slice(&borrow_ix.data[8..16]);
+        amount_borrowed = u64::from_le_bytes(borrowed_data)
+    } else {
+        return Err(ProtocolError::MissingBorrowIx.into());
+    }
+
+    // Add the fee to the amount borrowed (In our case we hardcoded it to 500 basis point)
+    let fee = (amount_borrowed as u128)
+        .checked_mul(500)
+        .unwrap()
+        .checked_div(10_000)
+        .ok_or(ProtocolError::Overflow)? as u64;
+    amount_borrowed = amount_borrowed
+        .checked_add(fee)
+        .ok_or(ProtocolError::Overflow)?;
+
+    // Transfer the funds from the protocol to the borrower
+    transfer(
+        CpiContext::new(
+            ctx.accounts.token_program.key(),
+            Transfer {
+                from: ctx.accounts.borrower_ata.to_account_info(),
+                to: ctx.accounts.protocol_ata.to_account_info(),
+                authority: ctx.accounts.borrower.to_account_info(),
+            },
+        ),
+        amount_borrowed,
+    )?;
+    Ok(())
+}
